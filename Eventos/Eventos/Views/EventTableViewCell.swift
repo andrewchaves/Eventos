6c569d58a324d9c94f56238ffca99d04bf82456d
Andrew do Vale <andrew.almeidachaves@gmail.com>
2020-01-17 15:24:33 -0400

    [Feature] Create the UITableViewCell Xib and Class.
    Signed-off-by: Andrew do Vale <andrew.almeidachaves@gmail.com>

---
+++ Eventos/Eventos/Views/EventTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EventTableViewCell.swift
+//  Eventos
+//
+//  Created by Andrew Chaves on 17/01/20.
+//  Copyright © 2020 Andrew Chaves. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var eventImageView: UIImageView!
+    @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var eventPriceLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        eventImageView.contentMode = .scaleAspectFit
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
