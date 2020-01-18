8aed4341d9008f48a1066aa2c332ce4f2baf6768
Andrew do Vale <andrew.almeidachaves@gmail.com>
2020-01-18 17:12:40 -0400

    [Feature] Added Checkin feature to application.
    Signed-off-by: Andrew do Vale <andrew.almeidachaves@gmail.com>

--- Eventos/Eventos/Controllers/EventDetailViewController.swift
+++ Eventos/Eventos/Controllers/EventDetailViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class EventDetailViewController: UIViewController {
     
+    // Event Detail components
     @IBOutlet weak var eventImageView: UIImageView!
     @IBOutlet weak var eventDescriptionTextView: UITextView!
     @IBOutlet weak var eventCheckinButton: UIButton!
     @IBOutlet weak var eventShareButton: UIButton!
     
+    // Pop-up components
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var eMailTextField: UITextField!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
     
     var eventVM: EventViewModel!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +36,66 @@ class EventDetailViewController: UIViewController {
     }
     
     private func setupView() {
+        
         self.title = eventVM.getTitle()
+        self.popUpView.isHidden = true
         self.eventImageView.image = eventVM.getImage()
         self.eventDescriptionTextView.text = eventVM.getDescription()
+        
+        eventCheckinButton.rx.tap.bind {
+            
+            self.popUpView.isHidden = false
+            
+        }.disposed(by: disposeBag)
+        
+        eventShareButton.rx.tap.bind {
+            print("Share tapped")
+        }.disposed(by: disposeBag)
+        
+        okButton.rx.tap.bind {
+            
+            self.makeCheckIn()
+            
+        }.disposed(by: disposeBag)
+        
+        cancelButton.rx.tap.bind {
+            
+            self.dissmissPopUp()
+            
+        }.disposed(by: disposeBag)
+    }
+    
+    private func makeCheckIn() {
+        if self.nameTextField.text != "" && self.eMailTextField.text  != "" {
+            
+            let name = self.nameTextField.text!
+            let email = self.eMailTextField.text!
+            
+            WebService().checkIn(name: name, email: email, eventId: self.eventVM.getId()) { result in
+
+               switch result {
+                   case .success(let response):
+                       print(response)
+                   case .failure(let error):
+                       print(error)
+               }
+            }
+        } else {
+            
+            self.feedbackLabel.text = "Preencha todos os campos!"
+            self.feedbackLabel.textColor = UIColor.red
+            
+        }
+    }
+    
+    private func dissmissPopUp() {
+        
+        self.feedbackLabel.text = "Preencha seus dados:"
+        self.feedbackLabel.textColor = UIColor.systemGray
+        self.nameTextField.text = ""
+        self.eMailTextField.text = ""
+        self.popUpView.isHidden = true
+        
     }
     
 }
--- Eventos/Eventos/Services/WebService.swift
+++ Eventos/Eventos/Services/WebService.swift
@@ -14,7 +14,12 @@ enum NetworkError: Error {
     case urlError
 }
 
+enum NetworkSuccess: Error {
+    case successPost
+}
+
 class WebService {
+    
     func loadEvents( completion: @escaping (Result<[Event], NetworkError>) -> Void){
         
         guard let eventsURL = URL(string: "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events?fbclid=IwAR0Ll0IuRnyQk4a-KBwi1JZDoxvUeilJEB0rJPsu5VRPK278NVC4LhWdhaQ") else {
@@ -37,6 +42,50 @@ class WebService {
                 completion(.failure(.decodingError))
             }
         }.resume()
+        
+    }
+    
+    func checkIn(name: String, email: String, eventId: String, completion: @escaping (Result<NetworkSuccess, NetworkError>) -> Void){
+        
+        guard let chekinURL = URL(string: "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events?fbclid=IwAR0Ll0IuRnyQk4a-KBwi1JZDoxvUeilJEB0rJPsu5VRPK278NVC4LhWdhaQ") else {
+            fatalError("URL incorrect.")
+        }
+        
+        let parameters = ["name":name,"email":email,"eventId":eventId]
+      
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        var request : URLRequest = URLRequest(url: chekinURL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+               print(error.localizedDescription)
+            completion(.failure(.decodingError))
+        }
+        let dataTask = session.dataTask(with: chekinURL) {
+            data,response,error in
+
+            guard let httpResponse = response as? HTTPURLResponse, let _ = data
+                else {
+                    completion(.failure(.decodingError))
+                    return
+            }
+            print(httpResponse.statusCode)
+            switch (httpResponse.statusCode) {
+                case 200:
+                    completion(.success(.successPost))
+                    break
+                case 400:
+                    completion(.failure(.domainError))
+                    break
+                default:
+                    break
+            }
+        }
+        dataTask.resume()
         
     }
 }
